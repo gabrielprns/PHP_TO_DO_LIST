be63a1972d38be501d11ec93357de94649cc76cd
gabrielprns <gabrielpedrornn@outlook.com>
2022-07-29 13:35:18 -0300

    CSS e atualizações

--- main.php
+++ main.php
@@ -5,6 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>To do List</title>
+  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
   <div class = "main-section">
@@ -18,7 +19,7 @@
   </div>
 
   <div class="show-all-section">
-    <div class = "All-itens">
+    <div class = "all-item">
       <input type="checkbox">
       <h2>pppppp</h2>
       <br>
