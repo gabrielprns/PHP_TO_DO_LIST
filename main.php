2f77949b83a41e57a4c9713ca58ff5e64fcb9c57
gabrielprns <gabrielpedrornn@outlook.com>
2022-07-28 21:59:43 -0300

    Project

--- main.php
+++ main.php
@@ -4,9 +4,20 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>To do List</title>
 </head>
 <body>
+  <div class = "main-section">
+  <div class = "add-section">
+    <form action="">
+      <input type="text"
+             name= "title"
+             placeholder = "É necessário preencher este campo" />
+      <button type="submit">Add &nbsp; <span>&#43</span></button>
+    </form>
+  </div>
+  
+</div>
   
 </body>
 </html>
