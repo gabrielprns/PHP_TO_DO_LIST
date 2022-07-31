b49fccbebd2963e8c5745f103527b0cd2482adf6
gabrielprns <gabrielpedrornn@outlook.com>
2022-07-30 22:03:42 -0300

    Adicionar tarefas

--- app/add.php
+++ app/add.php
@@ -7,6 +7,19 @@ if(isset($_POST['title'])){
 
   if(empty($title)){
     header("Location: ../main.php?mess=error");
-  }
+  }else {
+    $stmt = $conn->prepare("INSERT INTO todos(title) VALUE(?)");
+    $res = $stmt->execute([$title]);
+
+    if($res){
+        header("Location: ../main.php?mess=success"); 
+    }else {
+        header("Location: ../main.php");
+    }
+    $conn = null;
+    exit();
+}
+}else {
+    header("Location: ../main.php?mess=error");
 }
 ?>
