b23e420d61a9eff468426fb4ed42370f970bb55f
gabrielprns <gabrielpedrornn@outlook.com>
2022-07-30 21:32:15 -0300

    add

--- app/add.php
+++ app/add.php
@@ -1,1 +1,12 @@
-HELLO
+<?php
+
+if(isset($_POST['title'])){
+  require '../db_conn.php';
+
+  $title = $_POST['title'];
+
+  if(empty($title)){
+    header("Location: ../main.php?mess=error");
+  }
+}
+?>
